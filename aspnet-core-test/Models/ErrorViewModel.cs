94e1df925a06ca171dced90541914a5827ea4ad1
thara0214 <thara@archway.co.jp>
2020-05-22 13:39:51 +0900

    create project.

---
+++ aspnet-core-test/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aspnet_core_test.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
